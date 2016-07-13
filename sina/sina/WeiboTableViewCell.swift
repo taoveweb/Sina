1394b967b5cfb7294d56479d01fbaa6951323391
黄 家金 <hjj245148>
2016-07-13 08:08:52 +0800

    all

--- sina/sina/WeiboTableViewCell.swift
+++ sina/sina/WeiboTableViewCell.swift
@@ -11,13 +11,28 @@ import UIKit
 class WeiboTableViewCell: UITableViewCell {
 
     @IBOutlet weak var HeadImageView: UIImageView!
+    @IBOutlet weak var reposButton: UIButton!
+    @IBAction func repostAction(sender: UIButton) {
+        print("repost")
+    }
+    @IBOutlet weak var commentButton: UIButton!
+    @IBAction func commentAction(sender: UIButton) {
+        print("comment")
+    }
+    @IBOutlet weak var likeButton: UIButton!
+    @IBAction func likeAction(sender: UIButton) {
+        print("like")
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         
         self.HeadImageView.layer.cornerRadius=20
         self.HeadImageView.layer.masksToBounds=true
+        let headerTap=UITapGestureRecognizer(target: self, action: "HeaderTapAction")
     }
+    
+    func headerTapAction(
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
